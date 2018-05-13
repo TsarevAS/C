fa7eb347d739e9576633c0da07249038377b1cbd
TsarevAS <39243596+TsarevAS@users.noreply.github.com>
2018-05-14 00:12:14 +0300

    Лабораторная работа #0 Вариант 22

---
+++ Задача 2/Задача 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i = 22, y;
+            Console.WriteLine("Введите x");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите a");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите c");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите d");
+            double d = double.Parse(Console.ReadLine());
+            if ( Math.Abs(x) < 10 )
+             y = Math.Tan(x / i + a) - Math.Log(b * i + 7);
+            else
+               y = c * Math.Pow(Math.Pow(x, 2) + d * Math.Pow(i, 1.2), i);
+            Console.WriteLine("у =" + y);
+                    
+
+
+        }
+    }
+}
