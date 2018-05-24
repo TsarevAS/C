b8017d2830835838786763979188c1ba5c2564cd
TsarevAS <39243596+TsarevAS@users.noreply.github.com>
2018-05-24 19:38:37 +0300

    Add files via upload

---
+++ Задача 3(if)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_3_if_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую букву названия реки");
+            char k = char.Parse(Console.ReadLine());
+            if (k =='a')
+                Console.WriteLine("Страна, где протекает река - Россия");
+            else if ( k=='b')
+                Console.WriteLine("Страна, где протекает река - США");
+            else if (k=='c')
+                Console.WriteLine("Страна, где протекает река - Индия");
+            else if (k=='d')
+                Console.WriteLine("Страна, где протекает река - Китай");
+            else
+                Console.WriteLine("Ошибка");
+
+        }
+    }
+}
