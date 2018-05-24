92c3e417f0018a3c46770cc85cf8f14f619d0857
TsarevAS <39243596+TsarevAS@users.noreply.github.com>
2018-05-24 19:16:05 +0300

    Add files via upload

---
+++ Задача 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Введите первую букву названия реки");
+            char k = char.Parse(Console.ReadLine());
+            switch (k )
+            {
+                case  'a':
+                    Console.WriteLine("Страна,в которой протекает река - Россия");
+                    break;
+                case 'b':
+                    Console.WriteLine("Страна,в которой протекает река - США");
+                    break;
+                case  'c':
+                    Console.WriteLine("Страна,в которой протекает река - Индия");
+                    break;
+                case  'd':
+                    Console.WriteLine("Страна,в которой протекает река - Китай");
+                    break;
+                default:
+                    Console.WriteLine("Ошибка");
+                    break;
+            }
+        }
+    }
+}
